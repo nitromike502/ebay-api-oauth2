f1f88fe3a97c0ff8c4aaaee75e07d32675371f49
nitromike502 <nitromike502@gmail.com>
2020-08-20 19:43:30 -0700

    recommit

---
+++ src - Copy/src/Provider/Exception/EbayProviderException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ebay\OAuth2\Client\Provider\Exception;
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+
+class EbayProviderException extends IdentityProviderException
+{
+}
