c723a5ac7a4eb96e46be544dc0dd270fb2a45c90
nitromike502 <nitromike502@gmail.com>
2020-08-20 19:50:34 -0700

    accidental copies removed

--- src - Copy/Provider/Exception/EbayProviderException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ebay\OAuth2\Client\Provider\Exception;
-
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-
-class EbayProviderException extends IdentityProviderException
-{
-}
